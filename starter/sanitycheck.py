fc17a9b6d9d3b44480b54530ad5de7000835f712
anhndd3 <anhndd3@gmail.com>
2022-12-01 22:45:06 +0700

    Update sanitycheck.py file

--- starter/sanitycheck.py
+++ starter/sanitycheck.py
@@ -44,9 +44,10 @@ def run_sanity_check(test_dir):
         print(FAIL_COLOR+f"[{WARNING_COUNT}]")
         WARNING_COUNT += 1
         print(FAIL_COLOR+"No test cases were detected for the GET() method.")
-        print(FAIL_COLOR+'''\nPlease make sure you have a test case for the GET method.\
-                            This MUST test both the status code as well as
-                            the contents of the request object.\n''')
+        print(FAIL_COLOR + '\n' +
+              '''Please make sure you have a test case for the GET method.
+                 This MUST test both the status code as well as
+                 the contents of the request object.\n''')
         SANITY_TEST_PASSING = False
 
     else:
@@ -68,8 +69,9 @@ def run_sanity_check(test_dir):
         if not TEST_FOR_GET_METHOD_RESPONSE_BODY:
             print(FAIL_COLOR+f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+'''Your test case for GET() does not seem to be testing the CONTENTS
-                                of the response.\n''')
+            print(FAIL_COLOR +
+                  '''Your test case for GET() does not seem to be testing 
+                     the CONTENTS of the response.\n''')
 
     # POST()
     TEST_FOR_POST_METHOD_RESPONSE_CODE = False
@@ -81,9 +83,9 @@ def run_sanity_check(test_dir):
         WARNING_COUNT += 1
         print(FAIL_COLOR+"No test cases were detected for the POST() method.")
         print(FAIL_COLOR+'''Please make sure you have TWO test cases
-                            for the POST() method.''' +
-              '''\nOne test case for EACH of the possible inferences (results/outputs)
-                 of the ML model.\n''')
+                            for the POST() method.''' + '\n'
+              '''One test case for EACH of the possible inferences
+                (results/outputs) of the ML model.\n''')
         SANITY_TEST_PASSING = False
     else:
         if len(test_functions_for_post) == 1:
@@ -91,10 +93,11 @@ def run_sanity_check(test_dir):
             WARNING_COUNT += 1
             print(FAIL_COLOR+'''Only one test case was detected
                                 for the POST() method.''')
-            print(FAIL_COLOR+'''Please make sure you have two test cases
-                                for the POST() method.'''+'\n'
-                  '''One test case for EACH of the possible inferences (results/outputs)
-                     of the ML model.'''+'\n')
+            print(FAIL_COLOR +
+                  '''Please make sure you have two test cases
+                     for the POST() method.'''+'\n'
+                  '''One test case for EACH of the possible inferences
+                    (results/outputs) of the ML model.'''+'\n')
             SANITY_TEST_PASSING = False
 
         for func in test_functions_for_post:
@@ -123,7 +126,7 @@ def run_sanity_check(test_dir):
                 COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT < 2):
             print(FAIL_COLOR+f'[{WARNING_COUNT}]')
             WARNING_COUNT += 1
-            print(FAIL_COLOR+'''You do not seem to have TWO separate test cases,
+            print(FAIL_COLOR+'''You do not seem to have TWO separate test cases
                                 one for each possible prediction
                                 that your model can make.''')
 
