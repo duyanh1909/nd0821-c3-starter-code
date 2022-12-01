627210808d8d1e32b7e2d1f7e7cfb1faa8d96eef
anhndd3 <anhndd3@gmail.com>
2022-12-01 23:35:30 +0700

    Update app.py

--- starter/web/app.py
+++ starter/web/app.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from joblib import load
 
@@ -7,6 +8,12 @@ from pydantic import BaseModel
 from starter.ml.data import process_data
 from starter.ml.model import inference as predict
 
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 class User(BaseModel):
     age: int
