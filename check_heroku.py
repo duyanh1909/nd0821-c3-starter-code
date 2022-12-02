ac72bf890518ca76fc7ee0aaeed150d073a1ed3b
anhndd3 <anhndd3@gmail.com>
2022-12-02 09:00:57 +0700

    Add check_heroku.py to check Heroku Web

---
+++ check_heroku.py
@@ -0,0 +1,21 @@
+import requests
+
+
+data = {
+    "age": 31,
+    "workclass": "Private",
+    "fnlgt": 45781,
+    "education": "Masters",
+    "educationNum": 14,
+    "maritalStatus": "Never-married",
+    "occupation": "Prof-specialty",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Female",
+    "hoursPerWeek": 50,
+    "nativeCountry": "United-States"
+}
+
+
+response = requests.post("https://mlops-udacity-project.herokuapp.com/", json=data)
+print(f"""status: {response.status_code}\ndata: {response.json()}""")
